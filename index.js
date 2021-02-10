d4f1b897c19a74f5a077f1ae84629aad1a97273d
TSephanya Brun <tsbrun@icloud.com>
2021-02-10 11:48:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your code here
+// let formData = {
+//     "name": userName,
+//     "email": userEmail,
+// };
+
+// let configObj = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+// };
+
+function submitData(userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json" 
+        },
+        body: JSON.stringify(
+            {
+                "name": userName,
+                "email": userEmail
+            }
+        )
+    })
+    .then(response => response.json())
+    .then(body => {
+        document.querySelector('body').innerHTML = body["id"];
+    })
+    .catch(message => {
+        document.querySelector('body').innerHTML = message;
+    });
+};
